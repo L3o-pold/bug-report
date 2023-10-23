cabd4b06a5aa8c98a6c6a810840d2ac21f6d4008
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2023-10-23 20:12:59 +0200

    stream a video file in a video html tag

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/video', function () {
+    return response()->download(
+        resource_path('videos/TSU_640x360.mp4')
+    );
+});
